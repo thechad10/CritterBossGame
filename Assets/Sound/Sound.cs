80105c4f2ca3832dd9fec35312af3e1e8ec3487a
KeatHeat <97690772+KeatHeat@users.noreply.github.com>
2024-10-04 22:19:11 -0500

    Sound mixer creation

---
+++ Assets/Sound/Sound.cs
@@ -0,0 +1,24 @@
+using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+
+    public AudioSource source;
+
+    public AudioMixerGroup mixer;
+
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+
+    public bool loop;
+
+    public bool PlayAwake;
+
+
+
+}
---
+++ Assets/Sound/soundControl.cs
@@ -0,0 +1,57 @@
+using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class SoundControl : MonoBehaviour
+{
+    public bool stopPlay;
+    public Sound[] sounds;
+    public static SoundControl instance;
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.loop = s.loop;
+            s.source.playOnAwake = s.PlayAwake;
+            s.source.outputAudioMixerGroup = s.mixer;
+
+        }
+
+    }
+    public void Update()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source.volume = s.volume;
+        }
+    }
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+            return;
+        s.source.Play();
+    }
+    public void StopPlay(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+            return;
+        s.source.Stop();
+    }
+
+}
