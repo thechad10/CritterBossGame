2bb55517fc0f1c5dabb91fef938d19943d88b16d
Chad Compton <70557714+thechad10@users.noreply.github.com>
2024-10-07 12:26:19 -0500

    Pushing some tween John

--- Assets/Scripts/UI/EnableTween.cs
+++ Assets/Scripts/UI/EnableTween.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using UnityEngine.EventSystems;
+
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    [SerializeField] private GameObject button;
+    [SerializeField] EventSystem ui_Handler;
     [SerializeField] private RectTransform ui_Element;
     [SerializeField] private GameObject go;
     [SerializeField] private float startPos, endPos;
     private void OnEnable()
     {
+        ui_Handler.firstSelectedGameObject = button;
         ui_Element.DOAnchorPosY(endPos, 1);
         go.SetActive(false);
     }
