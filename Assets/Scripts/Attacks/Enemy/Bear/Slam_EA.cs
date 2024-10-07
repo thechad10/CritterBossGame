8d81de4ed70d45d12f4a8d627fd1092fc0f2573d
Nick-Len <nick.lenssen@trojans.dsu.edu>
2024-10-06 19:56:26 -0500

    NoMoreCrashing

--- Assets/Scripts/Attacks/Enemy/Bear/Slam_EA.cs
+++ Assets/Scripts/Attacks/Enemy/Bear/Slam_EA.cs
@@ -7,8 +7,8 @@ public class Slam_EA : EnemyAttack
 {
     [SerializeField]
     private ShakeData worldShake;
-    public bool bShake;
-    public bool bShaking;
+    private bool bShake;
+    private bool bShaking;
     private bool aTOb = false;
     private bool bTOc = false;
     [SerializeField] private float upTime = 1.5f;
@@ -25,7 +25,6 @@ public class Slam_EA : EnemyAttack
     [SerializeField] private float returnTime = 1.5f;
     [SerializeField] private float AcornSpawnTime = 1.5f;
     [SerializeField] private float AcornSpawnRate = 0.05f;
-    private bool AcornSpawning = false;
     [SerializeField] GameObject acornPrefab;
     private List<GameObject> acorns = new List<GameObject>();
     [SerializeField] private Transform spawnA;
@@ -50,85 +49,89 @@ public class Slam_EA : EnemyAttack
     }
     private void Update()
     {
-        if (aTOb)
+        if (IsAttacking)
         {
-            Timer += Time.deltaTime;
-            handsCollider.transform.position = Vector3.Lerp(posA.position, posB.position, Mathf.Clamp(Timer / upTime, 0, 1));
-            if (Timer >= upTime)
+            if (aTOb)
             {
-                Timer = 0;
-                aTOb = false;
-                bTOc = true;
-                handsCollider.enabled = true;
+                Timer += Time.deltaTime;
+                handsCollider.transform.position = Vector3.Lerp(posA.position, posB.position, Mathf.Clamp(Timer / upTime, 0, 1));
+                if (Timer >= upTime)
+                {
+                    Timer = 0;
+                    aTOb = false;
+                    bTOc = true;
+                    handsCollider.enabled = true;
+                }
             }
-        }
-        else if(bTOc)
-        {
-            Timer += Time.deltaTime;
-            handsCollider.transform.position = Vector3.Lerp(posB.position, posC.position, Mathf.Clamp(Timer / downTime, 0, 1));
-            if (Timer >= downTime)
+            else if(bTOc)
             {
-                Timer = 0;
-                bTOc = false;
-                slam = true;
-                bShake = true;
+                Timer += Time.deltaTime;
+                handsCollider.transform.position = Vector3.Lerp(posB.position, posC.position, Mathf.Clamp(Timer / downTime, 0, 1));
+                if (Timer >= downTime)
+                {
+                    Timer = 0;
+                    bTOc = false;
+                    slam = true;
+                    bShake = true;
+                }
             }
-        }
-        else if (slam)
-        {
-            Timer += Time.deltaTime;
-            if (Timer >= slamHoldTime)
+            else if (slam)
             {
-                slam = false;
-                returnDown = true;
-            }
+                Timer += Time.deltaTime;
+                if (Timer >= slamHoldTime)
+                {
+                    Timer = 0;
+                    slam = false;
+                    returnDown = true;
+                }
 
-        }
-        else if (returnDown)
-        {
-            Timer += Time.deltaTime;
-            handsCollider.enabled = false;
-            handsCollider.transform.position = Vector3.Lerp(posC.position, posA.position, Mathf.Clamp(Timer / returnTime, 0, 1));
-            if (Timer >= slamHoldTime)
-            {
-                slam = false;
-                returnDown = true;
             }
-        }
-        if (bShake)
-        {
-            bShake = false;
-            bShaking = true;
-            Shake();
-        }
-        if (bShaking)
-        {
-            Timer += Time.deltaTime;
-            acornTimer += Time.deltaTime;
-            if (acornTimer >= AcornSpawnRate)
+            else if (returnDown)
             {
-                acornTimer = 0;
-                acornSpawn();
+                Timer += Time.deltaTime;
+                handsCollider.enabled = false;
+                handsCollider.transform.position = Vector3.Lerp(posC.position, posA.position, Mathf.Clamp(Timer / returnTime, 0, 1));
+                if (Timer >= slamHoldTime)
+                {
+                    Timer = 0;
+                    slam = false;
+                    returnDown = false;
+                }
             }
+            if (bShake)
+            {
+                bShake = false;
+                bShaking = true;
+                Shake();
+            }
+            if (bShaking)
+            {
+                Timer += Time.deltaTime;
+                acornTimer += Time.deltaTime;
+                if (acornTimer >= AcornSpawnRate)
+                {
+                    acornTimer = 0;
+                    acornSpawn();
+                }
 
-            if (Timer >= AcornSpawnTime)
+                if (Timer >= AcornSpawnTime)
+                {
+                    bShaking = false;
+                    acornTimer = 0;
+                    Timer = 0;
+                }
+            }
+            else
             {
-                bShaking = false;
-                returnDown = true;
-                acornTimer = 0;
-                Timer = 0;
+                stopShake();
             }
         }
-        else
-        {
-            stopShake();
-            bear.FinishAttack();
-        }
+        
     }
 
     private void acornSpawn()
     {
-        acorns.Add(Instantiate(acornPrefab, new Vector2(0, spawnA.position.y), spawnB.rotation));
+        acorns.Add(Instantiate(acornPrefab, new Vector2(Random.Range(spawnA.position.x, spawnB.position.x), spawnA.position.y), spawnB.rotation));
     }
     private void Shake()
     {
@@ -138,6 +141,8 @@ public class Slam_EA : EnemyAttack
     private void stopShake()
     {
         CameraShakerHandler.Stop();
+        bear.FinishAttack();
+        handsCollider.enabled = false;
     }
 
 }
