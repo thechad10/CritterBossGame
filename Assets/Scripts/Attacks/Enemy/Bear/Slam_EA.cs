402e547942ee81d75a406829cd9cca1434580f1d
Nick-Len <nick.lenssen@trojans.dsu.edu>
2024-10-06 19:05:50 -0500

    BearSlam

--- Assets/Scripts/Attacks/Enemy/Bear/Slam_EA.cs
+++ Assets/Scripts/Attacks/Enemy/Bear/Slam_EA.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using FirstGearGames.SmoothCameraShaker;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Slam_EA : EnemyAttack
@@ -8,7 +9,34 @@ public class Slam_EA : EnemyAttack
     private ShakeData worldShake;
     public bool bShake;
     public bool bShaking;
+    private bool aTOb = false;
+    private bool bTOc = false;
+    [SerializeField] private float upTime = 1.5f;
+    [SerializeField] private float downTime = 0.5f;
+    private float Timer;
+    private float acornTimer = 0;
+    [SerializeField] private Collider2D handsCollider;
+    [SerializeField] private Transform posA;
+    [SerializeField] private Transform posB;
+    [SerializeField] private Transform posC;
+    private bool slam = false;
+    private bool returnDown = false;
+    [SerializeField] private float slamHoldTime = 1.5f;
+    [SerializeField] private float returnTime = 1.5f;
+    [SerializeField] private float AcornSpawnTime = 1.5f;
+    [SerializeField] private float AcornSpawnRate = 0.05f;
+    private bool AcornSpawning = false;
+    [SerializeField] GameObject acornPrefab;
+    private List<GameObject> acorns = new List<GameObject>();
+    [SerializeField] private Transform spawnA;
+    [SerializeField] private Transform spawnB;
+    private float spawnRange;
+    [SerializeField] private Bear bear;
 
+    private void OnEnable()
+    {
+        spawnRange = spawnB.position.x - spawnA.position.x;
+    }
     public override void Attack()
     {
         if (IsAttacking)
@@ -17,10 +45,56 @@ public class Slam_EA : EnemyAttack
             return;
         }
         IsAttacking = true;
-        
+        aTOb = true;
+        Timer = 0;
     }
     private void Update()
     {
+        if (aTOb)
+        {
+            Timer += Time.deltaTime;
+            handsCollider.transform.position = Vector3.Lerp(posA.position, posB.position, Mathf.Clamp(Timer / upTime, 0, 1));
+            if (Timer >= upTime)
+            {
+                Timer = 0;
+                aTOb = false;
+                bTOc = true;
+                handsCollider.enabled = true;
+            }
+        }
+        else if(bTOc)
+        {
+            Timer += Time.deltaTime;
+            handsCollider.transform.position = Vector3.Lerp(posB.position, posC.position, Mathf.Clamp(Timer / downTime, 0, 1));
+            if (Timer >= downTime)
+            {
+                Timer = 0;
+                bTOc = false;
+                slam = true;
+                bShake = true;
+            }
+        }
+        else if (slam)
+        {
+            Timer += Time.deltaTime;
+            if (Timer >= slamHoldTime)
+            {
+                slam = false;
+                returnDown = true;
+            }
+
+        }
+        else if (returnDown)
+        {
+            Timer += Time.deltaTime;
+            handsCollider.enabled = false;
+            handsCollider.transform.position = Vector3.Lerp(posC.position, posA.position, Mathf.Clamp(Timer / returnTime, 0, 1));
+            if (Timer >= slamHoldTime)
+            {
+                slam = false;
+                returnDown = true;
+            }
+        }
         if (bShake)
         {
             bShake = false;
@@ -29,14 +103,33 @@ public class Slam_EA : EnemyAttack
         }
         if (bShaking)
         {
+            Timer += Time.deltaTime;
+            acornTimer += Time.deltaTime;
+            if (acornTimer >= AcornSpawnRate)
+            {
+                acornTimer = 0;
+                acornSpawn();
+            }
 
+            if (Timer >= AcornSpawnTime)
+            {
+                bShaking = false;
+                returnDown = true;
+                acornTimer = 0;
+                Timer = 0;
+            }
         }
         else
         {
             stopShake();
+            bear.FinishAttack();
         }
     }
 
+    private void acornSpawn()
+    {
+        acorns.Add(Instantiate(acornPrefab, new Vector2(0, spawnA.position.y), spawnB.rotation));
+    }
     private void Shake()
     {
         CameraShakerHandler.Shake(worldShake);
