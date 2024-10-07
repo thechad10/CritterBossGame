869692032241a8224d3859f724f962a1dc8528d4
Nick-Len <nick.lenssen@trojans.dsu.edu>
2024-10-06 19:14:14 -0500

    beardothing

--- Assets/Bear.cs
+++ Assets/Bear.cs
@@ -1,6 +1,92 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class Bear : Boss
 {
+    [Header("Coyote Specific")]
+    [SerializeField, Tooltip("The amount of time it takes to execute the first attack")]
+    private float firstAttackDelay;
+    [SerializeField, Tooltip("The amount of time it takes to execute another attack")]
+    private float attackDelay;
+    [SerializeField, Tooltip("Attacks that are available in second phase")]
+    private List<EnemyAttack> secondPhaseAttacks = new();
+    private bool isSecondPhase = false;
+    private EnemyAttack chosenAttack;
+    private EnemyAttack cachedAttack;
+    private bool isBeginningOfFight = true;
+    private float startingTimer;
+    private Animator coyoteAnimator;
+    private string animName;
 
+    private void Start()
+    {
+        coyoteAnimator = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        if (isBeginningOfFight)
+        {
+            startingTimer += Time.deltaTime;
+
+            if (startingTimer >= firstAttackDelay)
+            {
+                isBeginningOfFight = false;
+                startingTimer = 0;
+            }
+            else
+            {
+                return;
+            }
+        }
+        if (IsDead) return;
+        if (HealthPercentage <= 0.5 && !isSecondPhase)
+        {
+            StartSecondPhase();
+        }
+
+        if (IsAttacking) return;
+        StartAttack();
+        int attackIndex;
+
+        if (!isSecondPhase) //first phase
+        {
+            do
+            {
+                attackIndex = UnityEngine.Random.Range(0, Attacks.Count);
+                chosenAttack = Attacks[attackIndex];
+            } while (cachedAttack == chosenAttack);
+        }
+        else //second phase
+        {
+            do
+            {
+                attackIndex = UnityEngine.Random.Range(0, secondPhaseAttacks.Count);
+                chosenAttack = secondPhaseAttacks[attackIndex];
+            } while (cachedAttack == chosenAttack);
+        }
+        cachedAttack = chosenAttack;
+
+        StartCoroutine(Attack(chosenAttack, attackIndex));
+    }
+
+    IEnumerator Attack(EnemyAttack eAttack, int attackAnim)
+    {
+        if (attackAnim == 0) animName = "Coyote_Swipe_High";
+        if (attackAnim == 1) animName = "Coyote_Pounce";
+        if (attackAnim == 2) animName = "Coyote_Swipe_Low";
+        if (attackAnim == 3) animName = "Coyote_Sling";
+
+        coyoteAnimator.Play(animName);
+        yield return new WaitForSeconds(attackDelay);
+        eAttack.Attack();
+    }
+
+    public void StartSecondPhase()
+    {
+        isSecondPhase = true;
+        Debug.Log("COYOTE HAS ENTERED SECOND PHASE!");
+    }
 }
