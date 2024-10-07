32efa7376da5fed34005744443d5c893a9b498ef
Obliterine <73033572+obliterine@users.noreply.github.com>
2024-10-06 21:43:33 -0500

    PauseMenu made

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -1,12 +1,13 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] private PlayerInput pInput;
     [SerializeField] private bool GamePaused = false;
-    [SerializeField] private GameObject pauseMenuUI;
+    [SerializeField] private GameObject pausePanelUI;
     [SerializeField] private GameObject GameUI;
     [SerializeField] private EventSystem UIButtonStart;
     [SerializeField] private GameObject pauseStart;
@@ -15,22 +16,51 @@ public class PauseMenu : MonoBehaviour
     {
         pInput = new PlayerInput();
         pInput.Enable();
-        pInput.Player.Pause.performed += Pause;
+        pInput.Player.Pause.performed += TryPause;
     }
     private void OnDisable()
     {
-        pInput.Player.Pause.performed -= Pause;
+        pInput.Player.Pause.performed -= TryPause;
         pInput.Disable();
     }
+
+    public void TryPause(InputAction.CallbackContext c)
+    {
+        if(GamePaused)
+        {
+            Resume();
+        }
+        else
+        {
+            Pause();
+        }
+    }
+
     public void Resume()
     {
-        pauseMenuUI.SetActive(false);
+        pausePanelUI.SetActive(false);
         GameUI.SetActive(true);
         Time.timeScale = 1f;
     }
-    public void Pause(InputAction.CallbackContext c)
+
+    public void Restart()
     {
-        pauseMenuUI.SetActive(true);
+        pInput.Player.Pause.performed -= TryPause;
+        pInput.Disable();
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void LevelSelect(string sceneName)
+    {
+        pInput.Player.Pause.performed -= TryPause;
+        pInput.Disable();
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(sceneName);
+    }
+    public void Pause()
+    {
+        pausePanelUI.SetActive(true);
         UIButtonStart.firstSelectedGameObject = pauseStart;
         GameUI.SetActive(false);
         Time.timeScale = 0f;
