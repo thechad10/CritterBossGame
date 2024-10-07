07f424be74a7ea690a4ac01d9f9f4382212f1287
Chad Compton <70557714+thechad10@users.noreply.github.com>
2024-10-06 20:59:31 -0500

    CoyoteBoss Game over

--- Assets/Scripts/UI/EnableTween.cs
+++ Assets/Scripts/UI/EnableTween.cs
@@ -1,16 +1,14 @@
 using UnityEngine;
-
+using UnityEngine.UI;
+using DG.Tweening;
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private RectTransform ui_Element;
+    [SerializeField] private GameObject go;
+    [SerializeField] private float startPos, endPos;
+    private void OnEnable()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        ui_Element.DOAnchorPosY(endPos, 1);
+        go.SetActive(false);
     }
 }
