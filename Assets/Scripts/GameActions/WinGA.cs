44b46679d5e22fd823a712779e7dfa6cafb5a9be
thechad10 <chadcomptonfilm@gmail.com>
2024-11-22 02:55:19 -0600

    Added Steamworks functionality and Achievement Capabilities

--- Assets/Scripts/GameActions/WinGA.cs
+++ Assets/Scripts/GameActions/WinGA.cs
@@ -11,12 +11,15 @@ public class WinGA : GameAction
         switch (winOption) {
             case option.coyoteWin:
                 levelProgression.CoyoteDefeated();
+                var co_complete = new Steamworks.Data.Achievement("coyote_slain");
                 break;
             case option.birdWin: 
                 levelProgression.BirdDefeated();
+                var bi_complete = new Steamworks.Data.Achievement("bird_slain");
                 break;
             case option.bearwin: 
                 levelProgression.BearDefeated();
+                var be_complete = new Steamworks.Data.Achievement("bear_slain");
                 break;
             default: 
                 break;
---
+++ Assets/Scripts/SteamIntegration.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class SteamIntegration : MonoBehaviour
+{
+    private void Start()
+    {
+        try
+        {
+            Steamworks.SteamClient.Init(3324230);
+            PrintYourName();
+        }
+        catch (System.Exception e)
+        {
+            //Something is not right
+            Debug.Log(e);
+        }
+    }
+    private void PrintYourName()
+    {
+        Debug.Log(Steamworks.SteamClient.Name);
+    }
+    private void Update()
+    {
+        Steamworks.SteamClient.RunCallbacks();
+    }
+    private void OnApplicationQuit()
+    {
+        Steamworks.SteamClient.Shutdown();
+    }
+    void InvokeCreditsAchievement()
+    {
+        var ach = new Steamworks.Data.Achievement("credits_visited");
+    }
+}
